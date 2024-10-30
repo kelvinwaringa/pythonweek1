32d4740b6f6e86c35625feb595caaa70aab9357a
Kelvin Waringa <waringakelvin4@gmail.com>
2024-10-30 15:56:12 +0300

    Add user input program to collect and display personalized message.
    - Prompt user for their name, age, and location
    - Print out a customized greeting message using user input

---
+++ weekone.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("What is your location? ")
+
+print(f"Hello, {name}! You are {age} years old, and you live in {location}. Nice to meet you {name}.")
